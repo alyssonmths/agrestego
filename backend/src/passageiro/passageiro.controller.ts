e7f56e63e85e5c597a312f42231b7b59fe3a0688
Alysson <alyssonmths5@gmail.com>
2026-05-29 17:47:39 -0300

    feat(): atualizar perfil de passageiro

--- backend/src/passageiro/passageiro.controller.ts
+++ backend/src/passageiro/passageiro.controller.ts
@@ -1,13 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put, Req, UseGuards } from '@nestjs/common';
 import { PassageiroService } from './passageiro.service';
 import { CreatePassageiroDto } from './dto/create-passageiro.dto';
+import { UpdatePassageiroDto } from './dto/update-passageiro.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('passageiro')
 export class PassageiroController {
-  constructor(private readonly passageiroService: PassageiroService) {}
+  constructor(private readonly passageiroService: PassageiroService) { }
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createPassageiroDto: CreatePassageiroDto) {
     return this.passageiroService.create(createPassageiroDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Put()
+  update(@Req() req: any, @Body() updatePassageiroDto: UpdatePassageiroDto) {
+    const userId = req.user?.userId;
+    return this.passageiroService.update(userId, updatePassageiroDto);
   }
 }
--- backend/src/passageiro/passageiro.service.ts
+++ backend/src/passageiro/passageiro.service.ts
@@ -1,19 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePassageiroDto } from './dto/create-passageiro.dto';
 import { PrismaService } from 'src/database/prisma.service';
+import { UpdatePassageiroDto } from './dto/update-passageiro.dto';
 
 @Injectable()
 export class PassageiroService {
 
   constructor(private readonly prisma: PrismaService) { }
 
-  create(createPassageiroDto: CreatePassageiroDto) {
-    return this.prisma.passageiro.create({
-      data: createPassageiroDto,
+  async create(request: CreatePassageiroDto) {
+    return await this.prisma.passageiro.create({
+      data: request,
     });
   }
 
-  findOne(email: string): Promise<any> {
-    return this.prisma.passageiro.findUnique({ where: { email } });
+  async findOne(email: string): Promise<any> {
+    return await this.prisma.passageiro.findUnique({ where: { email } });
+  }
+
+  async update(id: number, request: UpdatePassageiroDto) {
+    return await this.prisma.passageiro.update({
+      data: request,
+      where: { id }
+    });
   }
 }
