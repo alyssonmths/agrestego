a6b55fd9d610b3ef0be5d507916205aaf129d9de
Jesus Miguel <jesusalmenares04@gmail.com>
2026-06-08 21:20:48 -0300

    feat():Configuração de Swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('API Documentation')
+  .setDescription('API documentation for the application')
+  .setVersion('1.0.0')
+  .addBearerAuth()
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   app.enableCors({
     origin: process.env.CORS_ORIGIN,
